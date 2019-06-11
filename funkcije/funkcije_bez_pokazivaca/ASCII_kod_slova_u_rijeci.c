86f93b201176139b6ee41e59399e91f3a130c32b
win32mk <45974865+win32mk@users.noreply.github.com>
2019-06-11 07:30:35 +0200

    Create ASCII_kod_slova_u_rijeci.c

---
+++ funkcije/funkcije_bez_pokazivaca/ASCII_kod_slova_u_rijeci.c
@@ -0,0 +1,31 @@
+// (c) recycleTheC - TSRB 2019.
+// Učitati n slova i u funkciji ispisati ASCII kod ucitanog slova.
+
+#include<stdio.h>
+
+void ascii(char s);
+
+int main() {
+
+    int n, i;
+	char slovo;
+
+    printf("Upisi broj ucitavanja slova: ");
+    scanf("%d", &n);
+
+    for(i=0;i<n;i++){
+	    printf("Upisi %d. slovo: ", i+1);
+	    scanf(" %c", &slovo);
+
+	    ascii(slovo);
+    }
+
+	return 0;
+
+}
+
+void ascii(char s) {
+
+	printf("Slovo: %c, ASCII kod: %d\n", s, s);
+
+}
