ae2c2448cc87ff8d2ecf473b5b36187da8ae3fbe
win32mk <45974865+win32mk@users.noreply.github.com>
2019-08-10 07:59:46 +0200

    Update ispis_rijeci_koje_zapocinju_slovom.c

--- nizovi/ispis_rijeci_koje_zapocinju_slovom.c
+++ nizovi/ispis_rijeci_koje_zapocinju_slovom.c
@@ -7,7 +7,7 @@
 
 int main() {
 
-	int i, d;
+	int i, d, n=0;
 	char r[255];
 
 	printf("Upisi recenicu: ");
@@ -15,15 +15,31 @@ int main() {
 
 	d = strlen(r);
 
-	printf("Rijeci koje pocinju slovom a:\n");
-
 	for (i = 0; i < d; i++) {
 		if ((r[i] == 'a' || r[i] == 'A') && (r[i - 1] == ' ' || i == 0)) {
-			for (i; r[i] != ' ' && i < d; i++) {
-				printf("%c", r[i]);
-			}
-			printf("\n");
+			n++;
+			break;
 		}
 	}
+
+	if (n == 0) {
+		printf("U recenici nema rijeci koje zapocinju slovom 'a'!\n");
+	}
+
+	else {
+
+		printf("Rijeci koje pocinju slovom a:\n");
+
+		for (i = 0; i < d; i++) {
+			if ((r[i] == 'a' || r[i] == 'A') && (r[i - 1] == ' ' || i == 0)) {
+				for (i; r[i] != ' ' && i < d; i++) {
+					printf("%c", r[i]);
+				}
+				printf("\n");
+			}
+		}
+
+	}
+
 	return 0;
 }
